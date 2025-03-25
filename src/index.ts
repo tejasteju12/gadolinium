77ccea07646a6cf78461104b4b2ec9297e09b384
tejasteju12 <steja.1si21ad059@gmail.com>
2025-03-25 12:06:15 +0530

    Remove unused authentication types and update server route initialization

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
-import { hono } from "./routes";
+import { allRoutes } from "./routes/routes";
 import { serve } from "@hono/node-server";
 
-serve(hono, (info) => {
+serve(allRoutes, (info) => {
   console.log(`Server is running @ http://localhost:${info.port}`);
 });
